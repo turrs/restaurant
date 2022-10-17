f47832f0e17defeba7facd65c9d3fa870ee8fc05
turrs <faturrahmanavoiders@gmail.com>
2022-10-18 01:57:53 +0800

    fix: crud menu

--- api/controllers/menuController.js
+++ api/controllers/menuController.js
@@ -12,3 +12,76 @@ exports.findAll = (req, res) => {
       });
     });
 };
+
+exports.create = (req, res) => {
+  console.log(12, req.body);
+  const menu = new Menu({
+    name: req.body.name,
+    image: req.body.image,
+    price: req.body.price,
+  });
+
+  menu
+    .save(menu)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(409).send({
+        message: err.message || "Some error",
+      });
+    });
+};
+
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+  Menu.findById(id)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.status(409).send({
+        message: err.message || "cant catch id",
+      });
+    });
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Menu.findByIdAndRemove(id)
+    .then((result) => {
+      if (!result) {
+        res.status(404).send({
+          message: "Menu not Found",
+        });
+      }
+      res.send({
+        message: "menu was delete",
+      });
+    })
+    .catch((err) => {
+      res.status(409).send({
+        message: err.message || "cant catch id",
+      });
+    });
+};
+
+exports.update = (req, res) => {
+  const id = req.params.id;
+  Menu.findByIdAndUpdate(id, req.body)
+    .then((result) => {
+      if (!result) {
+        res.status(404).send({
+          message: "Menu not Found",
+        });
+      }
+      res.send({
+        message: "menu was update",
+      });
+    })
+    .catch((res) => {
+      res.status(409).send({
+        message: err.message || "cant catch id",
+      });
+    });
+};
--- api/routes/menu.routes.js
+++ api/routes/menu.routes.js
@@ -1,6 +1,12 @@
 module.exports = (app) => {
   const menus = require("../controllers/menuController");
   const router = require("express").Router();
+  var bodyParser = require("body-parser");
+  var jsonParser = bodyParser.json();
   router.get("/", menus.findAll);
+  router.post("/", jsonParser, menus.create);
+  router.get("/:id", menus.findOne);
+  router.delete("/:id", menus.delete);
+  router.put("/:id", jsonParser, menus.update);
   app.use("/api/menu", router);
 };
